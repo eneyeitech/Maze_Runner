13b24bf1ed06a3c4aa8278c5364f52aaf19312d6
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2022-03-19 16:13:19 +0100

    Stage 4/4: A way out

--- Maze Runner/task/src/maze/Main.java
+++ Maze Runner/task/src/maze/Main.java
@@ -1,10 +1,9 @@
 package maze;
 
 import java.io.*;
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
+
+
 public class Main {
     public static void main(String[] args) {
         // Many thanks to Mariano for the idea of using the debugMode flag
@@ -22,6 +21,7 @@ public class Main {
                     int mazeSize = scanner.nextInt();
                     maze = new Maze(mazeSize, debugMode);
                     maze.generate();
+                    maze.calculateMaze();
                     maze.displayMaze();
                     break;
                 case 2:
@@ -55,6 +55,12 @@ public class Main {
                     assert maze != null;
                     maze.displayMaze();
                     break;
+                case 5:
+                    assert maze != null;
+                    maze.walkMaze();
+                    maze.calculateMaze();
+                    maze.displayMaze();
+                    break;
                 case 0:
                     System.out.println("Bye !");
                     System.exit(0);
@@ -71,7 +77,8 @@ public class Main {
                 "2. Load a maze");
         if (hasMaze) {
             System.out.println("3. Save the maze\n" +
-                    "4. Display the maze");
+                    "4. Display the maze\n" +
+                    "5. Find the escape.");
         }
         System.out.println("0. Exit");
     }
@@ -80,10 +87,15 @@ public class Main {
 class Maze implements Serializable {
     private final int mazeSize;
     private final boolean debugMode;
+    int heightEdges;
+    private int[][] adjacencyMatrix;
     private int[][] minSpanTree;
     private int[][] maze; // the array of the maze to print
+    Set<Integer> resultSet; // The path through the maze will be on these nodes
+    boolean passed = false; // Initially, the maze was not passed
     final String WALL = "\u2588\u2588";
     final String EMPTY = "  ";
+    final String PASS = "//";
 
     public Maze(int mazeSize, boolean debugMode) {
         this.mazeSize = mazeSize;
@@ -91,13 +103,13 @@ class Maze implements Serializable {
     }
 
     public void generate() {
-        int heightEdges = (int) Math.ceil(((double) mazeSize - 2) / 2);
+        heightEdges = (int) Math.ceil(((double) mazeSize - 2) / 2);
         int weightEdges = (int) Math.ceil(((double) mazeSize - 2) / 2);
         if (debugMode) {
             System.out.println("Матрица узлов " + heightEdges + ":" + weightEdges);
         }
 
-        int[][] adjacencyMatrix = new int[heightEdges * weightEdges][heightEdges * weightEdges];
+        adjacencyMatrix = new int[heightEdges * weightEdges][heightEdges * weightEdges];
         // Fill 0
         for (int i = 0; i < adjacencyMatrix.length; i++) {
             for (int j = 0; j < adjacencyMatrix.length; j++) {
@@ -184,9 +196,11 @@ class Maze implements Serializable {
                 System.out.println();
             }
         }
+    }
 
-        // Prepare maze
-        // Lets fill it walls (1 = wall)
+    // Prepare maze
+    // Lets fill it walls (1 = wall)
+    public void calculateMaze() {
         maze = new int[mazeSize][mazeSize];
         for (int i = 0; i < mazeSize; i++) {
             for (int j = 0; j < mazeSize; j++) {
@@ -194,25 +208,34 @@ class Maze implements Serializable {
             }
         }
         // The entrance to the maze is always where be node #0
-        maze[1][0] = 0;
+        maze[1][0] = 2;
 
         int currentEdge = 0;
         int mazeRow = 1;
         int mazeCol = 1;
-        while (currentEdge < heightEdges * weightEdges) {
-            if (currentEdge + 1 < (currentEdge / weightEdges + 1) * weightEdges && minSpanTree[currentEdge][currentEdge + 1] == 1) {
-                maze[mazeRow][mazeCol] = 0;
+        while (currentEdge < heightEdges * heightEdges) {
+            if (currentEdge + 1 < (currentEdge / heightEdges + 1) * heightEdges && minSpanTree[currentEdge][currentEdge + 1] == 1) {
+                if (passed && resultSet.contains(currentEdge)) {
+                    maze[mazeRow][mazeCol] = 2;
+                } else {
+                    maze[mazeRow][mazeCol] = 0;
+                }
                 maze[mazeRow][mazeCol + 1] = 0;
                 maze[mazeRow][mazeCol + 2] = 0;
             }
-            if (currentEdge + weightEdges < heightEdges * weightEdges) {
-                if (minSpanTree[currentEdge][currentEdge + weightEdges] == 1) {
-                    maze[mazeRow][mazeCol] = 0;
+            if (currentEdge + heightEdges < heightEdges * heightEdges) {
+                if (minSpanTree[currentEdge][currentEdge + heightEdges] == 1) {
+                    if (passed && resultSet.contains(currentEdge)) {
+                        maze[mazeRow][mazeCol] = 2;
+                    } else {
+                        maze[mazeRow][mazeCol] = 0;
+                    }
                     maze[mazeRow + 1][mazeCol] = 0;
                     maze[mazeRow + 2][mazeCol] = 0;
+
                 }
             }
-            if (currentEdge + 1 <= (currentEdge / weightEdges + 1) * weightEdges - 1) {
+            if (currentEdge + 1 <= (currentEdge / heightEdges + 1) * heightEdges - 1) {
                 mazeCol += 2;
             } else {
                 mazeRow += 2;
@@ -221,11 +244,65 @@ class Maze implements Serializable {
 
             currentEdge++;
             // if the node was the last then the exit next to it
-            if (currentEdge == heightEdges * weightEdges) {
-                maze[mazeRow - 2][mazeSize - 1] = 0;
+            if (currentEdge == heightEdges * heightEdges) {
+                maze[mazeRow - 2][mazeSize - 1] = 2;
+            }
+        }
+
+        // Add way from edge to edge
+        for (int i = 0; i < mazeSize; i++) {
+            for (int j = 0; j < mazeSize; j++) {
+                // ->
+                if (maze[i][j] == 2 && j + 2 < mazeSize) {
+                    if (maze[i][j + 1] == 0 && maze[i][j + 2] == 2) {
+                        maze[i][j + 1] = 2;
+                    }
+                }
+                // -->
+                if (maze[i][j] == 2 && j + 3 < mazeSize) {
+                    if (maze[i][j + 1] == 0 && maze[i][j + 2] == 0 && maze[i][j + 3] == 2) {
+                        maze[i][j + 2] = 2;
+                    }
+                }
+                // V
+                if (maze[i][j] == 2 && i + 2 < mazeSize) {
+                    if (maze[i + 1][j] == 0 && maze[i + 2][j] == 2) {
+                        maze[i + 1][j] = 2;
+                    }
+                }
+                // ^
+                if (maze[i][j] == 2 && j - 2 > 0) {
+                    if (maze[i][j - 1] == 0 && maze[i][j - 2] == 2) {
+                        maze[i][j - 1] = 2;
+                    }
+                }
+                // ^-->
+                if (maze[i][j] == 2 && j + 2 < mazeSize && i - 2 > 0) {
+                    if (maze[i][j + 1] == 0 && maze[i][j + 2] == 0 && maze[i - 1][j + 2] == 0 && maze[i - 2][j + 2] == 2) {
+                        maze[i][j + 1] = 2;
+                        maze[i][j + 2] = 2;
+                        maze[i - 1][j + 2] = 2;
+                    }
+                }
+                // ^-->
+                if (maze[i][j] == 2 && j + 2 < mazeSize && i - 2 > 0) {
+                    if (maze[i][j + 1] == 0 && maze[i][j + 2] == 0 && maze[i - 1][j + 2] == 0 && maze[i - 2][j + 2] == 2) {
+                        maze[i][j + 1] = 2;
+                        maze[i][j + 2] = 2;
+                        maze[i - 1][j + 2] = 2;
+                    }
+                }
+                // ^<-
+                if (maze[i][j] == 2 && j - 1 > 0 && i - 2 > 0) {
+                    if (maze[i][j - 1] == 0 && maze[i - 1][j - 1] == 0 && maze[i - 2][j - 1] == 2) {
+                        maze[i][j - 1] = 2;
+                        maze[i - 1][j - 1] = 2;
+                    }
+                }
             }
         }
     }
+
 
     // Print maze
     public void displayMaze() {
@@ -233,12 +310,15 @@ class Maze implements Serializable {
             for (int j = 0; j < mazeSize; j++) {
                 if (maze[i][j] == 1) {
                     System.out.print(WALL);
+                } else if (maze[i][j] == 2 && passed) {
+                    System.out.print(PASS);
                 } else {
                     System.out.print(EMPTY);
                 }
             }
             System.out.println();
         }
+        passed = false;
     }
 
     // Saving the object using serialization
@@ -258,5 +338,57 @@ class Maze implements Serializable {
         Object obj = ois.readObject();
         ois.close();
         return obj;
+    }
+
+    // Pass of the Maze
+    public void walkMaze() {
+        // Use https://www.cs.usfca.edu/~galles/visualization/Dijkstra.html
+        // Undirected Graph & Adjacency Matrix Representation
+        char[] known = new char[minSpanTree.length];
+        int[] cost = new int[minSpanTree.length];
+        int[] path = new int[minSpanTree.length];
+        passed = true;
+        // Prepare arrays
+        Arrays.fill(cost, Integer.MAX_VALUE);
+        Arrays.fill(path, Integer.MAX_VALUE);
+
+        int index = 0;
+        known[0] = 'T';
+        cost[0] = 0;
+        path[0] = -1;
+        for (int j = 0; j < minSpanTree.length; j++) {
+            for (int i = 1; i < minSpanTree.length; i++) {
+                if (minSpanTree[index][i] > 0 && known[i] != 'T') {
+                    if (cost[i] > cost[index] + minSpanTree[index][i]) {
+                        cost[i] = cost[index] + minSpanTree[index][i];
+                        path[i] = index;
+                    }
+                }
+            }
+            int min = Integer.MAX_VALUE;
+            for (int i = 0; i < cost.length; i++) {
+                if (known[i] != 'T' && cost[i] < min) {
+                    min = cost[i];
+                    index = i;
+                }
+            }
+            known[index] = 'T';
+        }
+        if (debugMode) {
+            System.out.println("Vertex  Known  Cost  Path");
+            for (int i = 0; i < cost.length; i++) {
+                System.out.println("  " + i + "      " + known[i] + "    " + cost[i] + "   " + path[i]);
+            }
+        }
+        index = path.length - 1;
+        resultSet = new HashSet<>();
+        while (index != 0) {
+            resultSet.add(path[index]);
+            index = path[index];
+        }
+        if (debugMode) {
+            System.out.println("Путь:" + Arrays.toString(resultSet.toArray()));
+            System.out.println("К-во узлов:" + resultSet.size());
+        }
     }
 }
